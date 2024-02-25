3263eaaabf788fd0b40615843e98ef5a3ac668a4
buan0001 <bustermikkel@gmail.com>
2024-02-25 22:06:06 +0100

    get full course when creating

--- src/main/java/kea/exercise/hogwarts_api/services/CourseService.java
+++ src/main/java/kea/exercise/hogwarts_api/services/CourseService.java
@@ -1,6 +1,6 @@
 package kea.exercise.hogwarts_api.services;
 
-import jakarta.transaction.Transactional;
+//import jakarta.transaction.Transactional;
 import kea.exercise.hogwarts_api.dtos.CourseStudentsRequestDTO;
 import kea.exercise.hogwarts_api.dtos.StudentIdRequestDTO;
 import kea.exercise.hogwarts_api.models.Course;
@@ -8,25 +8,30 @@ import kea.exercise.hogwarts_api.models.Student;
 import kea.exercise.hogwarts_api.models.Teacher;
 import kea.exercise.hogwarts_api.repositories.CourseRepository;
 import kea.exercise.hogwarts_api.repositories.StudentRepository;
+import kea.exercise.hogwarts_api.repositories.TeacherRepository;
 import org.hibernate.Hibernate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
+@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 @Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
     private final StudentRepository studentRepository;
+    private final TeacherRepository teacherRepository;
 
-    public CourseService(CourseRepository courseRepository, StudentRepository studentRepository) {
+    public CourseService(CourseRepository courseRepository, StudentRepository studentRepository, TeacherRepository teacherRepository) {
         this.courseRepository = courseRepository;
         this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
     }
 
     public List<Course> findAll() {
@@ -47,10 +52,23 @@ public class CourseService {
         Optional<Course> course = courseRepository.findById(id);
         return course.map(Course::getStudents);
     }
-@Transactional()
+    //@Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public Optional<Course> save(Course newCourse) {
-        Course newOne = courseRepository.save(newCourse);
-        return courseRepository.findById(newOne.getId());
+        Course savedCourse = courseRepository.save(newCourse);
+
+        // tried multiple things, only this work around did the job :/
+        Teacher teacher = teacherRepository.findById(newCourse.getTeacher().getId()).orElse(null);
+        List<Student> students = studentRepository.findAllById(newCourse.getStudents().stream().map(Student::getId).toList());
+        //List<Student> students = studentRepository.findAllById(newCourse.getStudents().stream().map(Student::getId).collect(Collectors.toSet()));
+
+        savedCourse.setTeacher(teacher);
+        //Set<Student> studs = new HashSet<>(students);
+        savedCourse.setStudents(new HashSet<>(students));
+
+        return Optional.of(savedCourse) ;
+//        Course newOne = courseRepository.save(newCourse);
+//        return courseRepository.findById(newOne.getId());
+//        //return findById(newOne.getId());
     }
 
     public Optional<Course> updateFullCourse(int id, Course updatedCourse) {
